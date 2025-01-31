d44719ef5799a3e2daf6ee9c4fa1b8652b844fdd
Luca De Mauri <luca.demauri.2006@gmail.com>
2025-01-31 12:43:37 +0100

    aggiunta dei file associati alla pagina di  pratica

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('step')) {
-        alert('Hai cliccato su ' + event.target.textContent);
-        event.target.style.backgroundColor = 'lightgreen';
-      }
-})
