6c73148ae43a80a3dd66571ebc46c41dd5e8d8b5
Luca De Mauri <luca.demauri.2006@gmail.com>
2025-02-13 17:21:44 +0100

    ho fatto la barra

--- TagHunt/Componenti/BarraAvanzamento/script.js
+++ TagHunt/Componenti/BarraAvanzamento/script.js
@@ -1,21 +1,75 @@
 let xp = 0;
-const maxXp = 100; // Definisci il punteggio massimo per il livello
+let minXp = 0;
+let maxXp = 5000;
 const xpBar = document.getElementById('xp-bar');
 const xpText = document.getElementById('xp-text');
+const xpMin = document.getElementById('xp-min');
+minXp = parseInt(xpMin.textContent, 10);
+const xpMax = document.getElementById('xp-max');
+maxXp = parseInt(xpMax.textContent, 10);
+let liv1 = document.getElementById('liv-attuale');
+let livAttuale = parseInt(liv1.textContent, 10);
+let liv2 = document.getElementById('liv-successivo');
+let livSuccessivo = parseInt(liv2.textContent, 10);
 
-function updateXP(amount) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+async function updateXP(amount) {
   xp += amount;
-  if (xp > maxXp) xp = maxXp;  // Limita l'XP al massimo
-  const xpPercentage = (xp / maxXp) * 100;
+
+  if (xp > maxXp) {
+    xpPercentage = (xp / maxXp) * 100;
+    // Aggiorna la barra di progresso e il testo
+        xpBar.style.width = `${xpPercentage}%`;
+        xpText.textContent = `${xp}xp`;
+    await sleep(2000)
+        xpPercentage = 0;
+        xpBar.style.background = "#e0e0e0" ;
+        xpBar.style.width = `0`;
+    await sleep(500)
+    minXp = maxXp;
+    xpMin.textContent =  `${minXp}xp`
+    maxXp += 5000;
+    xpMax.textContent =  `${maxXp}xp`
+    livAttuale += 1;
+    livSuccessivo = livAttuale + 1;  
+    liv1.textContent = `${livAttuale}`;
+    liv2.textContent = `${livSuccessivo}`;  
+    xpPercentage = (xp / maxXp) * 100;
+        xpBar.style.background = "#4caf50" ;
+        xpBar.style.width = `${xpPercentage}%`;
+        xpText.textContent = `${xp}xp`;
+  }
+  else{
+    xpPercentage = (xp / maxXp) * 100;
+    // Aggiorna la barra di progresso e il testo
+    xpBar.style.width = `${xpPercentage}%`;
+    xpText.textContent = `${xp}xp`;
+  }
   
-  // Aggiorna la barra di progresso e il testo
-  xpBar.style.width = `${xpPercentage}%`;
-  xpText.textContent = `XP: ${xp} / ${maxXp}`;
+
+  
+
 }
 
 // Esempio di utilizzo: incrementa l'XP
-updateXP(20);  // Incrementa di 20 punti
+updateXP(2000);  // Incrementa di 20 punti
 setTimeout(function() {
-  updateXP(50);
+  updateXP(5000);
 }, 3000);
+setTimeout(function() {
+    updateXP(5000);
+  }, 3000);
+
+  setTimeout(function() {
+    updateXP(5000);
+  }, 6000);
+
+  setTimeout(function() {
+    updateXP(5000);
+  }, 7000);
+
     
